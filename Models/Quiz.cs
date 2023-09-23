4c738e3fb39ad27c1a3fef146b0752a807ac2697
Raed Alkhanbashi <raed.alkhanbashi@gmail.com>
2023-09-23 14:42:35 +0530

    Added Quiz Model.

---
+++ Models/Quiz.cs
@@ -0,0 +1,9 @@
+public class Quiz
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int CategoryID { get; set; }
+    public int CreatorUserID { get; set; }
+    public DateTime CreationDate { get; set; }
+}
