18f8d561f98ae2595016895203945ec36cffeb69
Raed Alkhanbashi <raed.alkhanbashi@gmail.com>
2023-09-23 14:48:13 +0530

    Added Answer Model.

---
+++ Models/Answer.cs
@@ -0,0 +1,9 @@
+public class Answer
+{
+    public int Id { get; set; }
+    public string Text { get; set; }
+    public bool IsCorrect { get; set; }
+    public int QuestionId { get; set; }
+    
+}
+
