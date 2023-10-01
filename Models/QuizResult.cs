ea6f2d103a7512ac2e73bec543fca7f576b0c3c4
Raed Alkhanbashi <raed.alkhanbashi@gmail.com>
2023-10-01 12:33:09 +0530

    Added QuizResult modal

---
+++ Models/QuizResult.cs
@@ -0,0 +1,14 @@
+namespace QuizOne.Models
+{
+    public class QuizResult
+    {
+        public int Id { get; set; }
+        public int QuizId { get; set; } 
+        public int UserId { get; set; }
+        public int Score { get; set; }
+        public DateTime CompletionDate { get; set; }
+        
+
+        public Quiz Quiz { get; set; }
+    }
+}
