93a1ac184fa2c2fd29e34d403012df4df5790e8a
Raed Alkhanbashi <raed.alkhanbashi@gmail.com>
2023-10-04 10:16:19 +0530

    Added Category Model

---
+++ Models/Category.cs
@@ -0,0 +1,10 @@
+namespace QuizOne.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+
+}
--- Models/Quiz.cs
+++ Models/Quiz.cs
@@ -5,9 +5,9 @@ namespace QuizOne.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public int CategoryID { get; set; }
         public int CreatorUserID { get; set; }
         public DateTime CreationDate { get; set; }
         public List<Question> Questions { get; set; }
+        public List<Category> Categories { get; set; }
     }
 }
