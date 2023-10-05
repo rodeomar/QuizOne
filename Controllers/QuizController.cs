c7e791edf0ed7e9a8b94e43727e618089d93c449
Raed Alkhanbashi <raed.alkhanbashi@gmail.com>
2023-10-05 10:29:27 +0530

    Added Start method

--- Controllers/QuizController.cs
+++ Controllers/QuizController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using QuizOne.Db;
-
+using QuizOne.Models;
 namespace QuizOne.Controllers
 {
     public class QuizController : Controller
@@ -8,7 +9,7 @@ namespace QuizOne.Controllers
         private readonly ApplicationDbContext _context;
 
         public QuizController(ApplicationDbContext context)
-        {   
+        {
             _context = context;
         }
 
@@ -16,5 +17,19 @@ namespace QuizOne.Controllers
         {
             return View(_context);
         }
+
+        public IActionResult Start(int QuizId)
+        {
+            List<Question> questions = _context.Questions.Include(q => q.QuizId == QuizId).ToList();
+            List<int> questionIds = questions.Select(q => q.Id).ToList();
+            List<Answer> answers = _context.Answers
+                .Where(a => questionIds.Contains(a.QuestionId))
+                .ToList();
+            Quiz? quiz = _context.Quizzes.Find(QuizId);
+
+
+            return View(new { Question = questions, Answer = answers, Quiz = quiz});
+        }
+
     }
 }
