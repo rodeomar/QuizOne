4cc172272fcef7389e250556360b06f061980b47
Raed Alkhanbashi <raed.alkhanbashi@gmail.com>
2023-10-05 09:49:57 +0530

    Added animations

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const cards = document.querySelectorAll('.customCard');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.style.opacity = 1;
+            entry.target.style.transform = 'translateX(0)';
+        }
+    });
+});
+
+cards.forEach(card => {
+    observer.observe(card);
+});
